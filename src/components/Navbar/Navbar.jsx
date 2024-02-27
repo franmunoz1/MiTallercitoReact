ad9a37d1bb755191b1bb5476b1c58c4ae5edaaf7
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2024-02-27 09:08:15 -0300

    put the number of the cart in the right of the cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -51,7 +51,7 @@ export function Navbar() {
                                                     key={item.name}
                                                     href={item.href}
                                                     className={classNames(
-                                                        item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-700 hover:text-white',
+                                                        item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-black hover:text-white',
                                                         'rounded-md px-3 py-2 text-sm font-medium'
                                                     )}
                                                     aria-current={item.current ? 'page' : undefined}
