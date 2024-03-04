dc772af082c8330639a9a42946b25142d25f9bf2
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2024-03-04 19:47:49 -0300

    adding promise to the proyect for loading products

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,7 @@
+const Item = () => {
+    return (
+        <div className="text-center">Item detail is coming soon</div>
+    )
+}
+
+export default Item
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,16 +1,15 @@
 /*eslint-disable react/prop-types*/
+import ItemList from '../ItemList/ItemList'
+import Item from '../Item/Item'
 import './ItemListContainer.css'
 
 const ItemListContainer = ({ greeting }) => {
 
-    const showContent = () => {
-        alert("Hola!")
-    }
-
     return (
         <>
             <h2 style={{ textAlign: 'center', color: 'grey', padding: '1rem' }}>{greeting}</h2>
-            <button className="rounded-full btnItemList d-flex mx-auto" onClick={(showContent)}>Pulsar para saludar</button>
+            <ItemList />
+            <Item />
         </>
 
     )
