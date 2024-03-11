02e29cae0536c8b7ebe63da822080284d9b84373
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2024-03-11 15:47:44 -0300

    error 404 page

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -1,6 +1,13 @@
 const Error404 = () => {
     return (
-        <div>Error404</div>
+        <div className="bg-gray-100 h-screen flex items-center justify-center">
+            <div className="text-center">
+                <h1 className="text-6xl font-bold text-red-500 mb-2">404</h1>
+                <p className="text-2xl font-semibold text-gray-800 mb-4">Página no encontrada</p>
+                <p className="text-lg text-gray-600 mb-8">La página que estás buscando no existe o ha sido eliminada.</p>
+                <a href="/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Volver a la página de inicio</a>
+            </div>
+        </div>
     )
 }
 
