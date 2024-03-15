5eb2ab1b6a6578acc5bb4e67c064aaffc994acd2
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2024-03-14 23:39:18 -0300

    creating checkout component

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,4 +1,10 @@
+import { CartContext } from "../../context/CartContext";
+import { useContext } from "react";
+
 const Checkout = () => {
+
+    const { cart, handleDelete, totalPrice } = useContext(CartContext);
+
     return (
         <div>Checkout</div>
     )
